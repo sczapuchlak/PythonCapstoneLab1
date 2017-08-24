4c80254f85f4812ab5ae9f02127ebaa8b031735e
stephanie zap <iamstephanie@gmail.com>
2017-08-24 16:14:16 -0500

    first 2 parts of lab done

---
+++ CamelCase.py
@@ -0,0 +1,27 @@
+#this program takes a users sentence and turns it into camel case
+#keeps the first letter lowercase
+print('Please enter a sentence in any sort of case! I will make it camel case for you!')
+#takes the users input and makes all of it lowercase, and then
+#capitalizes the first letter in each word
+sentence = raw_input().lower().title()
+#checks to see if any letters are anything but letters
+#displays warning for the user that the camel case may be incorrect
+for char in sentence :
+    if char.isalpha() == False:
+        print('You will not get the desired camelCase since there are symbols!')
+        raw_input('But I shall print for you anyways... press enter')
+        break
+#replaces the spaces with no spaces to the words are put together
+sentence= sentence.replace(' ','')
+#takes the first letter out and keeps the rest of the sentence
+firstLetterGone = sentence[1:]
+#then takes the first letter and lowercases it6
+makeFirstLetterLower = sentence[0].lower()
+#puts the lowercase first letter onto the sentenve again
+fullSentence= makeFirstLetterLower+firstLetterGone
+#displays the users sentence
+print(fullSentence)
+
+
+
+
